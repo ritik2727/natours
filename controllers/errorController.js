c0aa14a0aa674716a9dd7979d2c2917f593f61c8
ritik2727 <ritikjain2727@gmail.com>
2023-11-01 18:19:22 +0530

    handle mongoose vaildation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,7 +10,13 @@ const handleDuplicateErrorDB =(err)=>{
     const message = `Duplicate field value : ${value} .Please use another value`
     return new AppError(message, 400);
 }
- 
+const handleValidationErrorDB =(err)=>{
+    const error = Object.values(err.errors).map(el=>el.message);
+
+    const message  =`Invalid Input data . ${error.join('. ')}`
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -40,15 +46,18 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'developmentp') {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
+  } else if (process.env.NODE_ENV === 'development') {
     let error = { ...err };
     if (err.name === 'CastError') {
       error = handleCastErrorDB(error);
     }
     if(err.code === 11000){
         error = handleDuplicateErrorDB(error);
+    }
+    if(err.name === 'ValidationError'){
+        error = handleValidationErrorDB(error);
     }
     sendErrorProd(error, res);
   }
