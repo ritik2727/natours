19e1e5140f970bb072474e6c12c99ad1f0d23fcf
ritik2727 <ritikjain2727@gmail.com>
2023-11-08 13:24:48 +0530

    modeling tour guides child referesing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
-const userModel = require('./userModel')
+const userModel = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -102,10 +102,15 @@ const tourSchema = new mongoose.Schema(
         coordinates: [Number],
         address: String,
         description: String,
-        day:Number
+        day: Number,
       },
     ],
-    guides:Array
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref:'User'
+      },
+    ],
   },
   {
     toJSON: {
@@ -127,12 +132,6 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
-
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async id=>await userModel.findById(id));
-  this.guides = await Promise.all(guidesPromises)
-  next();
-})
 
 // tourSchema.post('save', function (doc, next) {
 //   console.log(doc);
