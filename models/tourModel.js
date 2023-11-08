d84f1b3d8aea41fd259862171bbedd0ca5e1232e
ritik2727 <ritikjain2727@gmail.com>
2023-11-08 13:19:05 +0530

    modeling tour guide embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const userModel = require('./userModel')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -104,6 +105,7 @@ const tourSchema = new mongoose.Schema(
         day:Number
       },
     ],
+    guides:Array
   },
   {
     toJSON: {
@@ -125,6 +127,12 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async id=>await userModel.findById(id));
+  this.guides = await Promise.all(guidesPromises)
+  next();
+})
 
 // tourSchema.post('save', function (doc, next) {
 //   console.log(doc);
