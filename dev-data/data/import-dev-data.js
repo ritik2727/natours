7b3ccc23bea4c326a13d008d8c128570c906f4cc
ritik2727 <ritikjain2727@gmail.com>
2023-11-08 13:09:41 +0530

    modeling location (geospatical data) using embedding in tours model

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -16,7 +16,7 @@ mongoose
 
 //   read json file
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
 );
 
 const importData = async () => {
--- models/tourModel.js
+++ models/tourModel.js
@@ -79,6 +79,31 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocations: {
+      // GeoJSON
+      // embedded object
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day:Number
+      },
+    ],
   },
   {
     toJSON: {
